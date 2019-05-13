567d4b502d220b74a75d8ecdb999baf6b38af3d3
hao wang <ge_nah@hotmail.com>
2019-05-13 18:12:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,12 @@ def happy_birthday(birthday_kids)
   end
 end
 
-
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids each do |key, value|
+    if value > 12
+      puts "You are too old for this."
+    else 
+      
+    end
+  end
+end
